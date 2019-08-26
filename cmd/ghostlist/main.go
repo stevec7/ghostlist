4bcec8902d900ee3b3f1a43528ff748925dd74a0
Steve Crusan <scrusan@kal.local>
2019-08-25 23:47:19 -0500

    renamed to follow the right format

---
+++ cmd/ghostlist/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/stevec7/ghostlist/pkg/ghostlist"
+	"log"
+)
+
+func main() {
+	expand := flag.String("expand", "host00[1-3]", "Expand the hostlist" )
+	flag.Parse()
+
+	result, err := ghostlist.ExpandHostList(*expand)
+
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+	fmt.Printf("%v\n", result)
+}
